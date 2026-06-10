8fcbf78063d5ad48cc9bf6318ea6d658ed969dfb
SANDARBH SINGH <148999720+5ANDAR8H-5INGH@users.noreply.github.com>
2026-06-10 19:22:26 +0530

    Leetcode Question 1011

---
+++ Question_1011.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int D) {
+        int mincap = 0;
+        int maxcap = 0;
+        for(int w : weights){
+            mincap = Math.max(w,mincap);
+            maxcap += w;
+        }
+        while(mincap < maxcap){
+            int mid = mincap + (maxcap-mincap)/2;
+            int days = 1;
+            int sum = 0;
+            for(int weight : weights){
+                if(sum + weight > mid){
+                    days++;
+                    sum = 0;
+                }
+                sum += weight;
+            }
+            if(days > D){
+                mincap = mid+1;
+            }
+            else{
+                maxcap = mid;
+            }
+        }
+        return mincap;
+    }
+}
